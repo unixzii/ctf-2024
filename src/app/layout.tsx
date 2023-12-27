ea5bd08bcff5bd884ff5addc21386bcdee49bfce
Cyandev <unixzii@gmail.com>
2023-12-28 02:19:04 +0800

    Add OG metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,10 +6,10 @@ import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
+export const metadata = {
   title: "CTF 2024",
   description: "New year CTF event by Cyandev",
-};
+} satisfies Metadata;
 
 export default function RootLayout({
   children,
@@ -21,6 +21,20 @@ export default function RootLayout({
       <head>
         <link rel="shortcut icon" href="/favicon.ico" />
         <link rel="icon" href="/favicon.png" />
+        <meta
+          property="twitter:image"
+          content="https://ctf.cyandev.app/og-card.png"
+        />
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:title" content={metadata.title} />
+        <meta property="twitter:description" content={metadata.description} />
+        <meta
+          property="og:image"
+          content="https://ctf.cyandev.app/og-card.png"
+        />
+        <meta property="og:title" content={metadata.title} />
+        <meta property="og:description" content={metadata.description} />
+        <meta property="og:url" content="https://ctf.cyandev.app/" />
       </head>
       <body className={inter.className}>
         <main className="flex min-h-screen flex-col items-center gap-12 p-24">
