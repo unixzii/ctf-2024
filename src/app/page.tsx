10f8e1c6c217ff38bf416b1e94c40c0ba3665030
Cyandev <unixzii@gmail.com>
2023-12-30 13:31:24 +0800

    Add tweet link in the final section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,8 +35,16 @@ export default function Home() {
         <div className="max-w-3xl w-full">
           <h2 className="text-5xl font-bold mb-4">🎉</h2>
           <p>
-            Congratulations. You’ve found the final flag! Repost this website
-            via Twitter to receive your reward. You can also check out your rank{" "}
+            Congratulations. You’ve found the final flag!{" "}
+            <a
+              className="text-blue-500 hover:underline"
+              target="_blank"
+              href="https://twitter.com/intent/tweet?text=I%20just%20finished%20the%20challenge%20of%20CTF%202024!%20%40unixzii%0ATry%20it%20here%3A%20https%3A%2F%2Fctf.cyandev.app"
+            >
+              Tweet
+            </a>{" "}
+            this website to receive your reward. You can also check out your
+            rank{" "}
             <Link className="text-blue-500 hover:underline" href="/ranking">
               here
             </Link>
